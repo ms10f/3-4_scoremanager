37acbf9a7011001e62ccb521a10417137eaeb651
ms10f <kmm2459346@stu.o-hara.ac.jp>
2025-04-24 09:52:21 +0900

    Actionにログイン必須かを指定出来るように

--- src/tool/Action.java
+++ src/tool/Action.java
@@ -4,5 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Action {
+    /**
+     * ログイン必須ページかどうかを返す
+     * 
+     * @return
+     */
+    public default boolean loginRequire() {
+        return false;
+    }
+
     public String execute(HttpServletRequest request, HttpServletResponse response);
 }
