1ea230765bd79ce546a58abb4fade9b894dfc7a7
ms10f <kmm2459346@stu.o-hara.ac.jp>
2025-04-24 11:25:47 +0900

    JSPに投げる用の名前付きエラーリストクラスの作成

---
+++ src/utils/NamedErrors.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NamedErrors {
+    private final Map<String, List<String>> errors = new HashMap<>();
+
+    public NamedErrors() {
+    }
+
+    public NamedErrors add(String name, String message) {
+        List<String> errors = this.errors.getOrDefault(name, new ArrayList<>());
+        errors.add(message);
+
+        this.errors.put(name, errors);
+
+        return this;
+    }
+
+    public Map<String, List<String>> get() {
+        return this.errors;
+    }
+}
